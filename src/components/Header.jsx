1beaf6e02b2629a54d2bdf25a8a4939968c7f532
clara seijo <clasei@proton.me>
2024-09-16 15:57:18 +0200

    project initial setup and components done

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+
+  return (
+    <div>
+        <h1 className="hero-title">Imagine a place...</h1>
+        <h4 className="hero-text">...where you can belong to a school club, a gaming group, 
+          or a global chess team. Discord is great for playing games and chilling with friends, 
+          or even building a worldwide community. 
+          Customize your own space to talk, play, and hang out.</h4>
+      </div>
+  )
+
+}
+
+export default Header
